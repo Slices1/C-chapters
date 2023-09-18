2cb1c6e904ac6ff228288460172cebd7a05f3888
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <145350645+BHASVIC-jonahwilsontroy23@users.noreply.github.com>
2023-09-18 13:52:52 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    float val1, val2 = 0;
-    char operatorchoice='x';
-    float output =0;
-    printf("input 2 floats seperated by commas: \n");
-    scanf("%f,%f",&val1,&val2);
-    printf("do you want to multiply (*), divide (/), add (+), subtract (-)");
-    scanf(" %c",&operatorchoice);
-    if( operatorchoice == '*' ) {
-        output=val1*val2;
-    } else if( operatorchoice == '/') {
-        output=val1/val2;
-    } else if( operatorchoice == '+') {
-        output=val1+val2;
-    } else if( operatorchoice == '-') {
-        output=val1-val2;
-    } else {
-        printf("wat\n");
+    char choice = "";
+    int childtickets = 0;
+    int adulttickets = 0;
+    float childcost =0;
+    float adultcost =0;
+
+    printf("Input for a weekday (w), weekend (s), or bank holiday (b): \n");
+    scanf("%c",&choice);
+    printf("Input the amount of adult tickets and child tickets desired (seperated by comams): \n");
+    scanf("%d,%d",&adulttickets,&childtickets);
+    if (choice == 'w') {
+        childcost =5*childtickets;
+        adultcost =8*adulttickets;
+    } else if (choice == 's') {
+        childcost =7.5*childtickets;
+        adultcost =12*adulttickets;
+    } else if (choice == 'b') {
+        childcost = 7.5 * childtickets;
+        adultcost = 14 * adulttickets;
     }
-    printf("Equation: %f %c %f\n",val1,operatorchoice,val2);
-    printf("Output: %f",output);
+    printf("Child ticket(s) amount: %d\n Adult ticket(s) amount: %d\n Child ticket(s) cost: %f\n Adult ticket(s) cost: %f\n Total cost: %f", childtickets, adulttickets, childcost, adultcost, childcost+adultcost);
+
     return 0;
 }
