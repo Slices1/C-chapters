43a851ab05396471d983b9ff2fe8ca95be703141
BHASVIC-jonahwilsontroy23 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-09-25 13:13:28 +0100

    mon3-finito

--- Animal-akinator/main.c
+++ Animal-akinator/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//testmon1
+    
 int main() {
     int choice =0;
     char ans = "x";
@@ -8,120 +8,117 @@ int main() {
     {
         printf("%s\n", array[i]);
     }
-    printf("Is your animal a mammal? (y/n): ");
-    scanf("%c",&ans);
-    if (ans == "y") {
+    printf("Is your animal a mammal? (y/n):");
+    scanf(" %c",&ans);
+    printf("DEBUG: %c",ans);
+    if (ans == 'y') {
         choice = 9;
-        printf("Native to English soil? (y/n): ");
-        scanf("%c",&ans);
-        if (ans == "y") {
+        printf("Native to English soil? (y/n):");
+        scanf(" %c",&ans);
+        if (ans == 'y') {
             choice = 15;
-            printf("Is it a great human companion? (y/n): ");
-            scanf("%c",&ans);
-            if (ans == "y") {
+            printf("Is it a great human companion? (y/n):");
+            scanf(" %c",&ans);
+            if (ans == 'y') {
                 choice = 18;
-                printf("Is it a house pet? (y/n): ");
-                scanf("%c",&ans);
-                if (ans == "y") {
+                printf("Is it a house pet? (y/n):");
+                scanf(" %c",&ans);
+                if (ans == 'y') {
                     choice = 19;
-                    printf("Is it man's best friend? (y/n): ");
-                    scanf("%c",&ans);
-                    if (ans == "y") {
+                    printf("Is it man's best friend? (y/n):");
+                    scanf(" %c",&ans);
+                    if (ans == 'y') {
                         choice = 20;
                     }
                 }
             } else {
-                choice = 18;
-                printf("Not Fluffy? (y/n): ");
-                scanf("%c",&ans);
-                if (ans == "y") {
-                    choice = 16;
-                    printf("Central to Pepper Pig? (y/n): ");
-                    scanf("%c",&ans);
-                    if (ans == "y") {
-                        choice = 17;
+                choice = 17;
+                printf("Fluffy? (y/n):");
+                scanf(" %c",&ans);
+                if (ans == 'n') {
+                    choice = 15;
+                    printf("Central to Pepper Pig? (y/n):");
+                    scanf(" %c",&ans);
+                    if (ans == 'y') {
+                        choice = 16;
                     }
                 }
             }
         } else {
-            printf("Sea-faring? (y/n): ");
-            scanf("%c",&ans);
-            if (ans == "y") {
-                printf("Humungous? (y/n): ");
-                scanf("%c",&ans);
-                if (ans == "y") {
-                    choice = 15;
+            printf("Sea-faring? (y/n):");
+            scanf(" %c",&ans);
+            if (ans == 'y') {
+                printf("Humungous? (y/n):");
+                scanf(" %c",&ans);
+                if (ans == 'y') {
+                    choice = 14;
                 } else {
-                    choice = 13;
-                    printf("Has whiskers? (y/n): ");
-                    scanf("%c",&ans);
-                    if (ans == "y") {
-                        choice = 14;
+                    choice = 12;
+                    printf("Has whiskers? (y/n):");
+                    scanf(" %c",&ans);
+                    if (ans == 'y') {
+                        choice = 13;
                     }
                 }
             } else {
-                printf("Able to kill you in an instant? (y/n): ");
-                scanf("%c",&ans);
-                choice = 10;
-                if (ans == "y") {
-                    printf("In MGM Pictures' branding? (y/n): ");
-                    scanf("%c",&ans);
-                    choice = 11;
-                    if (ans == "y") {
-                        choice = 12;
+                printf("Able to kill you in an instant? (y/n):");
+                scanf(" %c",&ans);
+                choice = 9;
+                if (ans == 'y') {
+                    printf("In MGM Pictures' branding? (y/n):");
+                    scanf(" %c",&ans);
+                    choice = 10;
+                    if (ans == 'y') {
+                        choice = 11;
                     }
                 }
             }
         }
     } else {
-        printf("Insect? (y/n): ");
-        scanf("%c",&ans);
-        if (ans == "y") {
-            printf("Flies? (y/n): ");
-            scanf("%c",&ans);
-            if (ans == "y") {
-                printf("Farmed for honey? (y/n): ");
-                scanf("%c",&ans);
-                choice = 8;
-                if (ans == "y") {
-                    choice = 9;
+        printf("Insect? (y/n):");
+        scanf(" %c",&ans);
+        if (ans == 'y') {
+            printf("Flies? (y/n):");
+            scanf(" %c",&ans);
+            if (ans == 'y') {
+                printf("Farmed for honey? (y/n):");
+                scanf(" %c",&ans);
+                choice = 7;
+                if (ans == 'y') {
+                    choice = 8;
                 }
             } else {
-                printf("6 legs? (y/n): ");
-                scanf("%c",&ans);
-                choice = 5;
-                if (ans == "y") {
-                    printf("Snacks on wood? (y/n): ");
-                    scanf("%c",&ans);
-                    choice = 6;
-                    if (ans == "y") {
-                        choice = 7;
+                printf("6 legs? (y/n):");
+                scanf(" %c",&ans);
+                choice = 4;
+                if (ans == 'y') {
+                    printf("Snacks on wood? (y/n):");
+                    scanf(" %c",&ans);
+                    choice = 5;
+                    if (ans == 'y') {
+                        choice = 6;
                     }
                 }
             }
         } else {
-            printf("Sea-faring? (y/n): ");
-            scanf("%c",&ans);
-            choice = 1;
-            if (ans == "y") {
-                choice = 2;
-                printf("2 eyes? (y/n): ");
-                scanf("%c",&ans);
-                if (ans == "y") {
-                    choice = 3;
-                    printf("Tentacle-y? (y/n): ");
-                    scanf("%c",&ans);
-                    choice = 3;
-                    if (ans == "y") {
-                        choice = 4;
+            printf("Sea-faring? (y/n):");
+            scanf(" %c",&ans);
+            choice = 0;
+            if (ans == 'y') {
+                choice = 1;
+                printf("2 eyes? (y/n):");
+                scanf(" %c",&ans);
+                if (ans == 'y') {
+                    choice = 2;
+                    printf("Tentacle-y? (y/n):");
+                    scanf(" %c",&ans);
+                    if (ans == 'y') {
+                        choice = 3;
                     }
                 }
             }
         }
     }
-
-
-
 
     printf("%s\n", array[choice]);
     return 0;
