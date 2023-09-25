6ecdf900377e9986be5b91b25e06150aec485132
BHASVIC-jonahwilsontroy23 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-09-25 14:01:24 +0100

    monn8

--- item-adder-task2-proper/main.c
+++ item-adder-task2-proper/main.c
@@ -3,20 +3,21 @@
 int main() {
     int item_count =0;
     int specified_item_no =0;
-    int ans=0;
+    int running_total=0;
+    int ans =0;
+    float mean = 0;
     printf("How many items r we addin today?!\n");
     scanf("%d",&specified_item_no);
-    int numbers[specified_item_no];
     do {
         printf("input item:");
         fflush(stdin);
         scanf("%d",&ans);
-        numbers[item_count] = ans;
+        running_total = running_total + ans;
         item_count ++;
     } while (item_count != specified_item_no);
 
-    for (int i = 0; i < specified_item_no; i++) {
-        printf("%d, ", numbers[i]);
-    }
+    float mean = running_total/specified_item_no
+
+    printf("Average value: %f",mean);
     return 0;
 }
