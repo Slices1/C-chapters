602941e515eaad5a89604c5527d0c2bc6ed344e0
BHASVIC-jonahwilsontroy23 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-09-25 13:30:13 +0100

    mon6

--- condition-controlled-loops/main.c
+++ condition-controlled-loops/main.c
@@ -2,5 +2,19 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    int error=0,age=0;
+
+    while (error != 1) {
+        fflush(stdin);
+        printf("Please enter your age:");
+        error = scanf("%d",&age);
+    }
+    if (age<=100) {
+        printf("You have %d years TO LIVE!!!",100-age);
+    } else {
+        printf("You should have died %d years ago :0",age-100);
+    }
+
     return 0;
 }
