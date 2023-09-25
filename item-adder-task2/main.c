81890554783ad9eb20c8ff63f47c4576ad27cbbe
BHASVIC-jonahwilsontroy23 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-09-25 13:53:31 +0100

    mon7

--- item-adder-task2/main.c
+++ item-adder-task2/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int item_count =0;
+    int specified_item_no =0;
+    int ans=0;
+    printf("How many items r we addin today?!\n");
+    scanf("%d",&specified_item_no);
+    int numbers[specified_item_no];
+    do {
+        printf("input item:");
+        fflush(stdin);
+        scanf("%d",&ans);
+        numbers[item_count] = ans;
+        item_count ++;
+    } while (item_count != specified_item_no);
+
+    for (int i = 0; i < specified_item_no; i++) {
+        printf("%s\n", numbers[i]);
+    }
     return 0;
 }
