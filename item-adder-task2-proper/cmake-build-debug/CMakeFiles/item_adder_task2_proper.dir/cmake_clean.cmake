b0b5640c4dd6212adc29c030b44e913a7f4b9dad
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-09-26 09:26:02 +0100

    tue1

---
+++ item-adder-task2-proper/cmake-build-debug/CMakeFiles/item_adder_task2_proper.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/item_adder_task2_proper.dir/main.c.obj"
+  "item_adder_task2_proper.exe"
+  "item_adder_task2_proper.exe.manifest"
+  "item_adder_task2_proper.pdb"
+  "libitem_adder_task2_proper.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/item_adder_task2_proper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- item-adder-task2-proper/main.c
+++ item-adder-task2-proper/main.c
@@ -5,7 +5,7 @@ int main() {
     int specified_item_no =0;
     int running_total=0;
     int ans =0;
-    float mean = 0;
+    float item_av =0;
     printf("How many items r we addin today?!\n");
     scanf("%d",&specified_item_no);
     do {
@@ -16,8 +16,8 @@ int main() {
         item_count ++;
     } while (item_count != specified_item_no);
 
-    float mean = running_total/specified_item_no;
+    item_av = running_total/specified_item_no;
 
-    printf("Average value: %f",mean);
+    printf("Average value: %f",item_av);
     return 0;
 }
