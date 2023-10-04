295348361acf893504cd390f0c559d80915d8a6d
BHASVIC-jonahwilsontroy23 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-10-04 08:57:08 +0100

    wedidkanymore

--- random-number-game/main.c
+++ random-number-game/main.c
@@ -18,8 +18,6 @@ checkPrime(int N) {
 }
 
 
-
-
 int main() {
     srand(time(NULL));
     int guess;
@@ -30,31 +28,39 @@ int main() {
     do {
         win_multiplier = 1;
         do {
-            printf("How much would you like to bet. (You have %d credits available): ",credits);
-            scanf("%d",bet);
+            printf("\nHow much would you like to bet. (You have %d credits available): ",credits);
+            scanf("%d",&bet);
         } while (bet>credits || bet<0);
 
         int random_number = rand() % 30 + 1;
 
-        printf("Guess my noomber!!! you little insert homer simpson strangling bart clip.\n debug: %d", random_number);
+        printf("\nGuess my noomber!!! you little insert homer simpson strangling bart clip.\n debug: %d\n", random_number);
         scanf("%d", &guess);
+
+        credits -= bet;
         if (guess == random_number) {
+            printf("JACKPOOOOOOOOOOOOOOOOOOOOOOOOOOOOt (you didnt lose any moneY)!\n");
             if (random_number % 10 ==0) {
                 win_multiplier *=3;
+                printf("Congrats bro, you hit a multiple of 10! payout x3");
             }
             if (checkPrime(random_number) == 1) {
                 win_multiplier *=5;
+                printf("Congrats bro, you hit a prime number! payout x5");
             }
             if (random_number < 5) {
                 win_multiplier *=2;
+                printf("Congrats bro, you hit a number below 5! payout x2");
             }
+            credits += bet*win_multiplier;
         } else {
-            credits -= bet;
+            printf("boohoooo :,(");
         }
 
-        printf("Credits now total: %d\nCash out? (y=yes, else=no)",credits);
-        scanf("%c",&loopydoopy);
+        printf("\nCredits now total: %d\nCash out? (y=yes, else=no)",credits);
+        scanf(" %c",&loopydoopy);
+        fflush(stdin);
     } while (credits>0 && loopydoopy != 'y');
-    printf("You cashed out.");
+    printf("You cashed out with %d credits.",credits);
     return 0;
 }
