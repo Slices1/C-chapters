eb7a27e08d288f300cd13d1a03dc8859e8e179f1
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-11-01 08:59:59 +0000

    afsfdfd

---
+++ The-Wiggles-Kashyyyk-hotel/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+//function area
+//...
+
+
+
+
+
+//main
+int main() {
+    //declaring variables and other shiz
+    char choice = 'x';
+    char name[64] = {'a','b'};
+    char surname[64];
+
+    //
+    //hotel program loop
+    while (1) {
+        printf("\nChoose a function (i to check in, o to check out, or b to book a table): ");
+        scanf("%c",&choice);
+
+        if (choice == 'i') {          // check in
+
+
+        } else if (choice == 'o') {   // check out
+
+
+        } else if (choice == 'b') {   // booking a table
+
+        }
+
+
+        //this is the end of while loop
+    }
+    return 0;
+}
+//notes section:
+//ben will try to break out code, so make sure to add validation checks!
+
+
+//test
+//hello there ed and christian
