62cdd1eb3331393ba1e8b5d9849315188acfbb6b
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-11-01 11:22:17 +0000

    kashyyyk-hotel

--- The-Wiggles-Kashyyyk-hotel/main.c
+++ The-Wiggles-Kashyyyk-hotel/main.c
@@ -4,7 +4,61 @@
 
 
 
+char checkIn() {
+    //ed
+    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
+    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
 
+    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0" or "exit" i think :/
+
+    //task list:
+    //
+
+
+    return userCharArray and the userIntArray somehow..
+}
+
+char checkOut() {
+    //christian
+    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
+    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
+
+    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0" or "exit" i think :/
+
+
+    //task list:
+    //calculate total cost of room
+    // calculate  total cost of parties board (meal)
+    //if had daily newspapers add 5.50
+    //if user age > 65 10% discount on the room rate
+    // if any guests are 16 and under 50% discount on board rate for them
+
+    //show users booing id and main guests name
+    //each of the subtotals amounts frm amove where it is tleast >0
+    //overall bill
+
+    /
+
+    //nice job!
+
+
+
+    return userCharArray and the userIntArray somehow..
+}
+
+char bookTable() {
+    //jonah
+    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
+    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
+
+    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0" or "exit" i think :/
+
+
+    //task list:
+    // store tables, tatooine, endor, naboo
+
+    return userCharArray and the userIntArray somehow..
+}
 
 //main
 int main() {
@@ -17,21 +71,33 @@ int main() {
     //hotel program loop
     while (1) {
         printf("\nChoose a function (i to check in, o to check out, or b to book a table): ");
-        scanf("%c",&choice);
+        scanf("%c", &choice);
 
         if (choice == 'i') {          // check in
             //ed
-
+            printf("\n>Checking in...");
+            checkIn();
+            //set each char array term in the desired profile to the function's
+            //set each int array term in the desired profile to the function's
 
         } else if (choice == 'o') {   // check out
             //christian
-
+            printf("\n>Checking out...");
+            checkOut();
+            //set each char array term in the desired profile to the function's
+            //set each int array term in the desired profile to the function's
 
         } else if (choice == 'b') {   // booking a table
             //jonah
+            printf("\n>Booking in...");
+            bookTable();
+            //set each char array term in the desired profile to the function's
+            //set each int array term in the desired profile to the function's
 
-
+        } else {                   //invalid input
+            printf("\n>Invalid Input");
         }
+
 
 
         //this is the end of while loop
@@ -41,8 +107,24 @@ int main() {
 
 //notes section:
 //ben will try to break out code, so make sure to add validation checks!
+//need to have the profile data stored on the computer itself.
 
 
 //test
 //hello there ed and christian
 // hello there
+//car
+//tractor
+//jesus                         chic fil a
+//balls
+//chicken fingers
+// // //
+//
+
+int main2() {   // for testing purposes
+    char userProfile_char[which profile][which var][stores th rest strings];
+    int userProfile_int[profiles][which variable];
+    char tables[][]
+
+    return 0;
+}
